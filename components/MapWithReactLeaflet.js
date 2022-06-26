bf3706d4f571538c6581402f1a057cded9d3f291
quantumomid <omid.wakili@outlook.com>
2022-06-26 12:32:30 +0100

    add react-leaflet basic implementation

---
+++ components/MapWithReactLeaflet.js
@@ -0,0 +1,29 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useMap } from "react-leaflet/hooks";
+import Script from "next/script";
+
+export default function MapWithReactLeaflet () {
+    return (
+        <>
+            <div id="wrapper" className="relative h-full">
+                <MapContainer id="map-container" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+                {/* <div id="map" className="h-[400px] w-full"></div> */}
+            </div>
+            <Script
+                src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+                integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+                crossOrigin=""
+            />
+        </>
+    )
+}
